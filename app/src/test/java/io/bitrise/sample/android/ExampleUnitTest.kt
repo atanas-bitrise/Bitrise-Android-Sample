665c28ee5152d34f622da52164aa21354b95c596
Atanas Chanev <atanas.chanev@bitrise.io>
2022-07-15 13:27:23 +0100

    Temporary folder rule

--- app/src/test/java/io/bitrise/sample/android/ExampleUnitTest.kt
+++ app/src/test/java/io/bitrise/sample/android/ExampleUnitTest.kt
@@ -1,17 +1,32 @@
 package io.bitrise.sample.android
 
 import org.junit.Test
-
+import org.junit.Rule
+import org.junit.rules.*
 import org.junit.Assert.*
+
+import java.io.File
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExperimentalCoroutinesApi
 class ExampleUnitTest {
+
+    @get:Rule val temporaryFolder = TemporaryFolder()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    
+    @Test
+    fun temporaryFolder_isCreated() {
+        val f: File = temporaryFolder.newFile()
+        print(f.name)
+        assertEquals(42, 41+2)
     }
 }

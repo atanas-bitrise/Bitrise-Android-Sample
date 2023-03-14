6915a3e2ca83d1721617af65cce33a633468a154
atanas-bitrise <84032001+atanas-bitrise@users.noreply.github.com>
2023-03-14 11:35:57 +0100

    Do not remove unused symbol

--- app/src/main/java/io/bitrise/sample/android/MainActivity.kt
+++ app/src/main/java/io/bitrise/sample/android/MainActivity.kt
@@ -2,11 +2,13 @@ package io.bitrise.sample.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val someStringToken = "MySecretStringTokenThatMustBeHiddenAndNotAvailableToAnybody"
+        Log.d("TAG", someStringToken)
     }
 }
